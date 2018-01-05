b01227edd7bd38840308583cb645d5f69aa00dd9
arhn17 <32793336+arhn17@users.noreply.github.com>
2018-01-05 10:07:11 +0700

    login

---
+++ app/src/main/java/com/example/admin/program2/main/Login.java
@@ -0,0 +1,15 @@
+package com.example.admin.program2.main;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.admin.program2.R;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
