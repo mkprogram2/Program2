f7be9dcf5f8ae630af11b1f9e0f5c3ca0419482b
arhn17 <32793336+arhn17@users.noreply.github.com>
2018-01-17 10:48:56 +0700

    renew

--- app/src/main/java/com/example/admin/program2/service/EmployeeService.java
+++ app/src/main/java/com/example/admin/program2/service/EmployeeService.java
@@ -7,10 +7,13 @@ import com.example.admin.program2.model.person;
 import java.util.HashMap;
 import java.util.List;
 
+import okhttp3.RequestBody;
 import retrofit2.Call;
+import retrofit2.http.Body;
 import retrofit2.http.GET;
 import retrofit2.http.Header;
 import retrofit2.http.POST;
+import retrofit2.http.PUT;
 import retrofit2.http.Path;
 
 /**
@@ -32,6 +35,7 @@ public interface EmployeeService
     @GET("roles")
     Call<List<Role>> GetRoles();
 
-    @POST("persons/")
-
+    @PUT("persons/")
+    Call<person> PutEmployee(@Header("person") String person,
+                             @Body RequestBody persons);
 }
